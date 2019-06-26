ee36a04df97aee403da1793b8afcd7a61b961a72
Luanderson Nascimentos dos Santos <Luanderson Santos>
2019-06-25 22:35:16 -0400

    Api on

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 var onLine = 'http://agiliza-com-br.umbler.net/'
 var offLine = 'http://localhost:3000'
 const api = axios.create({
-    baseURL: offLine
+    baseURL: onLine
 })
 
 export default api
